455b40b0dfed4d938decdd458427cf9706e67486
Ian Wright <49083526+Landaman@users.noreply.github.com>
2025-02-25 13:48:36 -0500

    Add mode back to the statusline

--- lua/options.lua
+++ lua/options.lua
@@ -12,9 +12,6 @@ vim.opt.number = true
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
 
--- Don't show the mode, since it's already in the status line
-vim.opt.showmode = false
-
 -- Sync clipboard between OS and Neovim.
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
