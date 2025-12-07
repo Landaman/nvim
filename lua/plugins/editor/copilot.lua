5c8e416d7acfb7017b5c38ad813e2ea50a6a47c1
Ian Wright <49083526+Landaman@users.noreply.github.com>
2025-12-07 15:14:33 -0800

    Switch to copilot.lua
    Way less CPU intensive

--- lua/plugins/editor/copilot.lua
+++ lua/plugins/editor/copilot.lua
@@ -1,5 +1,37 @@
 return {
-  'github/copilot.vim',
-  event = vim.g.lazy_file,
-  cond = not vim.g.vscode,
+  'zbirenbaum/copilot.lua',
+  cmd = 'Copilot',
+  build = ':Copilot auth',
+  event = 'BufReadPost',
+  opts = {
+    suggestion = {
+      auto_trigger = true,
+      keymap = {
+        accept = '<Tab>',
+        accept_word = '<M-Right>',
+        accept_line = '<M-C-Right>',
+        next = '<M-]>',
+        prev = '<M-[>',
+        dismiss = '<C-]>',
+      },
+    },
+    panel = { enabled = false },
+  },
+  config = function(_, opts)
+    vim.api.nvim_create_autocmd('User', {
+      pattern = 'BlinkCmpMenuOpen',
+      callback = function()
+        vim.b.copilot_suggestion_hidden = true
+      end,
+    })
+
+    vim.api.nvim_create_autocmd('User', {
+      pattern = 'BlinkCmpMenuClose',
+      callback = function()
+        vim.b.copilot_suggestion_hidden = false
+      end,
+    })
+
+    require('copilot').setup(opts)
+  end,
 }
