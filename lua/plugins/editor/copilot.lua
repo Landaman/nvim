675a252a4f603e5a1281c0cb1c76dd2995201ef2
Ian Wright <49083526+Landaman@users.noreply.github.com>
2025-12-21 19:28:05 -0600

    Always show copilot
    Hiding it during blink completion doesn't really work and is actually
    pretty annoying

--- lua/plugins/editor/copilot.lua
+++ lua/plugins/editor/copilot.lua
@@ -17,21 +17,4 @@ return {
     },
     panel = { enabled = false },
   },
-  config = function(_, opts)
-    vim.api.nvim_create_autocmd('User', {
-      pattern = 'BlinkCmpMenuOpen',
-      callback = function()
-        vim.b.copilot_suggestion_hidden = true
-      end,
-    })
-
-    vim.api.nvim_create_autocmd('User', {
-      pattern = 'BlinkCmpMenuClose',
-      callback = function()
-        vim.b.copilot_suggestion_hidden = false
-      end,
-    })
-
-    require('copilot').setup(opts)
-  end,
 }
